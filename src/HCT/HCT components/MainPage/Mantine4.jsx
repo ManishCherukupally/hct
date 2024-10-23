979efee59c8a4dad443636655c7f0ca63c324754
Manish C <manish@tridemobility.com>
2024-10-23 10:34:21 +0530

    title registration

--- src/HCT/HCT components/MainPage/Mantine4.jsx
+++ src/HCT/HCT components/MainPage/Mantine4.jsx
@@ -13,7 +13,7 @@ import facebookImage1 from '../../../assets/colored-instagram-logo-new.png';
 
 const Mantine4 = () => {
   const [opened, { open, close }] = useDisclosure(false);
-  const isMobile = useMediaQuery('(max-width: 800px)'); 
+  const isMobile = useMediaQuery('(max-width: 800px)');
 
   const form = useForm({
     initialValues: {
@@ -33,67 +33,67 @@ const Mantine4 = () => {
 
   return (
     <>
-      <Modal opened={opened} onClose={close} title="Authentication" centered>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            <TextInput
-              withAsterisk
-              label="Name"
-              placeholder="Enter your Name"
-              {...form.getInputProps('name')}
-              mb='xs' radius='md'
-            />
-            <TextInput
-              withAsterisk
-              label="Category"
-              placeholder="enter category"
-              {...form.getInputProps('category')}
-              mb='xs' radius='md'
-            />
-            <TextInput
-              withAsterisk
-              mask="+91 (000) 000-00-00"
-              label="phone number"
-              placeholder="Your phone number"
-              {...form.getInputProps('phone')} mb='xs' radius='md'
-            />
-            <TextInput
-              withAsterisk
-              label="Email"
-              placeholder="your@email.com"
-              {...form.getInputProps('email')} mb='xs' radius='md'
-            />
-            <TextInput
-              withAsterisk
-              label="Location"
-              placeholder="your location"
-              {...form.getInputProps('location')} mb='xs' radius='md'
-            />
-            <NumberInput
-              placeholder="Your age"
-              label="Your age"
-              withAsterisk
-              {...form.getInputProps('age')} mb='xs' radius='md'
-            />
-            <Radio.Group
-              name="favoriteFramework"
-              label="Gender"
-              withAsterisk
-              {...form.getInputProps('gender')} mb='xs' radius='md'
-              style={{ color: 'blue' }}
-            >
-              <Group mt="xs">
-                <Radio value="male" label="Male" />
-                <Radio value="female" label="Female" />
-                <Radio value="other" label="Other" />
-              </Group>
-            </Radio.Group>
-
-            <Group position="right" mt="md">
-              <Button type="submit">Submit</Button>
+      <Modal opened={opened} onClose={close} title="Registration" centered>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput
+            withAsterisk
+            label="Name"
+            placeholder="Enter your Name"
+            {...form.getInputProps('name')}
+            mb='xs' radius='md'
+          />
+          <TextInput
+            withAsterisk
+            label="Category"
+            placeholder="enter category"
+            {...form.getInputProps('category')}
+            mb='xs' radius='md'
+          />
+          <TextInput
+            withAsterisk
+            mask="+91 (000) 000-00-00"
+            label="phone number"
+            placeholder="Your phone number"
+            {...form.getInputProps('phone')} mb='xs' radius='md'
+          />
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="your@email.com"
+            {...form.getInputProps('email')} mb='xs' radius='md'
+          />
+          <TextInput
+            withAsterisk
+            label="Location"
+            placeholder="your location"
+            {...form.getInputProps('location')} mb='xs' radius='md'
+          />
+          <NumberInput
+            placeholder="Your age"
+            label="Your age"
+            withAsterisk
+            {...form.getInputProps('age')} mb='xs' radius='md'
+          />
+          <Radio.Group
+            name="favoriteFramework"
+            label="Gender"
+            withAsterisk
+            {...form.getInputProps('gender')} mb='xs' radius='md'
+            style={{ color: 'blue' }}
+          >
+            <Group mt="xs">
+              <Radio value="male" label="Male" />
+              <Radio value="female" label="Female" />
+              <Radio value="other" label="Other" />
             </Group>
-          </form>
+          </Radio.Group>
+
+          <Group position="right" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
       </Modal>
-      
+
 
       <BackgroundImage src="https://wallpapercave.com/wp/wp8298483.jpg" style={{ height: isMobile ? '150vh' : '95vh' }}>
         <Grid h={isMobile ? 'auto' : '125vh'} align='center' opacity={0.9} >
@@ -172,61 +172,61 @@ const Mantine4 = () => {
 
             <Footer mt={isMobile ? '-0.5rem' : '12.9rem'} height={isMobile ? "9rem" : '9rem'} pt="1.5rem" style={{ backgroundColor: '#FBD40B' }}>
               <Container style={{ display: 'flex', alignItems: 'center', padding: 0 }}>
-               
+
                 <Link to="https://www.facebook.com/people/Sai-Teja/100063960496461/?mibextid=LQQJ4d" target='_blank'>
-        <img
-          src={facebookImage} 
-          alt="Facebook"
-          style={{width: '4.4rem', height: '100%', objectFit: 'cover'  }} 
-        />
-      </Link>
-      <Link to="https://www.instagram.com/healthcoach_saiteja/?igshid=YmMyMTA2M2Y%3D" target='_blank'>
-        <img
-          src={facebookImage1} 
-          alt="Instagram"
-          style={{ width: '2.5rem', height: '99.8%', objectFit: 'cover' }} 
-        />
-      </Link>
-    
-
-    {/* Button Section */}
-    <div style={{ marginLeft: 'auto' }}>
-      <Button
-      mr={'2rem'}
-      mb={'1rem'}
-        radius="xl"
-        size="xl"
-        component="a"
-        mt="8px"
-        styles={(theme) => ({
-          root: {
-            backgroundColor: '#1F3469',
-            color: 'white',
-            padding: '1rem 2.5rem',
-            fontSize: '18px',
-            fontWeight: 700,
-            borderRadius: '30px',
-            boxShadow:
-              '10px 0 40px rgba(255, 253, 208, 1), -10px 0 40px rgba(255, 253, 208, 1), 0 0 30px rgba(255, 253, 208, 0.8)',
-            transition: 'background-color 0.3s ease, box-shadow 0.3s ease, transform 0.3s ease',
-            '&:hover': {
-              backgroundColor: '#FBD40B',
-              boxShadow:
-                '10px 0 40px rgba(255, 253, 208, 1), -10px 0 40px rgba(255, 253, 208, 1), 0 0 30px rgba(255, 253, 208, 0.8)',
-              transform: 'scale(1.05)',
-            },
-          },
-        })}
-        onClick={open}
-      >
-        Register Here
-      </Button>
-    </div>
-  </Container>
-</Footer>
+                  <img
+                    src={facebookImage}
+                    alt="Facebook"
+                    style={{ width: '4.4rem', height: '100%', objectFit: 'cover' }}
+                  />
+                </Link>
+                <Link to="https://www.instagram.com/healthcoach_saiteja/?igshid=YmMyMTA2M2Y%3D" target='_blank'>
+                  <img
+                    src={facebookImage1}
+                    alt="Instagram"
+                    style={{ width: '2.5rem', height: '99.8%', objectFit: 'cover' }}
+                  />
+                </Link>
 
 
-      </Grid.Col>
+                {/* Button Section */}
+                <div style={{ marginLeft: 'auto' }}>
+                  <Button
+                    mr={'2rem'}
+                    mb={'1rem'}
+                    radius="xl"
+                    size="xl"
+                    component="a"
+                    mt="8px"
+                    styles={(theme) => ({
+                      root: {
+                        backgroundColor: '#1F3469',
+                        color: 'white',
+                        padding: '1rem 2.5rem',
+                        fontSize: '18px',
+                        fontWeight: 700,
+                        borderRadius: '30px',
+                        boxShadow:
+                          '10px 0 40px rgba(255, 253, 208, 1), -10px 0 40px rgba(255, 253, 208, 1), 0 0 30px rgba(255, 253, 208, 0.8)',
+                        transition: 'background-color 0.3s ease, box-shadow 0.3s ease, transform 0.3s ease',
+                        '&:hover': {
+                          backgroundColor: '#FBD40B',
+                          boxShadow:
+                            '10px 0 40px rgba(255, 253, 208, 1), -10px 0 40px rgba(255, 253, 208, 1), 0 0 30px rgba(255, 253, 208, 0.8)',
+                          transform: 'scale(1.05)',
+                        },
+                      },
+                    })}
+                    onClick={open}
+                  >
+                    Register Here
+                  </Button>
+                </div>
+              </Container>
+            </Footer>
+
+
+          </Grid.Col>
 
         </Grid>
 
