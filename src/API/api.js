71b136c640c315fc019adbe4992234bfbb0f7819
Mrdeveloper2 <pavan0221kalyan@gmail.com>
2025-02-04 14:27:06 +0530

    updatedCode

--- src/API/api.js
+++ src/API/api.js
@@ -3,12 +3,12 @@ axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'x-csrftoken'
 const client = axios.create(
-    // {
-    //     baseURL: 'https://hct.automactechnologies.in/'
-    // }
     {
-        baseURL: 'http://192.168.29.220:8000/'
+        baseURL: 'https://hct.automactechnologies.in/'
     }
+    // {
+    //     baseURL: 'http://192.168.29.220:8000/'
+    // }
 )
 
 export default client
